24e915748008fc60b958257f94cd3dd515bc6771
Danyil <danilagarma1999@mail.ru>
2026-01-11 15:46:53 +0200

    dd pagination, change api endpoint

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,15 +4,19 @@ import NewsBanner from "../../components/NewsBanner/NewsBanner"
 import classes from "./style.module.css"
 import NewsList from "../../components/NewsList/NewsList"
 import Skeleton from "../../components/Skeleton/Skeleton"
+import Pagination from "../../components/Pagination/Pagination"
 
 const Main = () => {
     const [news, setNews] = useState([]);
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalPages = 10;
+    const pageSize = 10;
 
     useEffect(() => {
-        const fetchNews = async () => {
+        const fetchNews = async (currentPage) => {
             try {
-                const response = await getNews();
+                const response = await getNews(currentPage, pageSize);
                 console.log(response)
                 setIsLoading(false)
                 setNews(response.news)
@@ -20,12 +24,25 @@ const Main = () => {
                 console.log(error)
             }
         }
-        fetchNews()
-    }, [])
+        fetchNews(currentPage)
+    }, [currentPage])
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) setCurrentPage(currentPage + 1)
+    }
+    const handlePrevPage = () => {
+        if (currentPage > 1) setCurrentPage(currentPage - 1)
+    }
+    const handlePageClick = (pageNumber) => {
+        setCurrentPage(pageNumber)
+    }
 
     return (
         <main className={classes.main}>
             {news.length && !isLoading ? <NewsBanner item={news[0]} /> : <Skeleton type={"banner"} count={1} />}
+            <Pagination totalPages={totalPages}
+                handleNextPage={handleNextPage} handlePrevPage={handlePrevPage} handlePageClick={handlePageClick} currentPage={currentPage}
+            />
             {!isLoading ? <NewsList news={news} /> : <Skeleton type={"item"} count={10} />}
         </main>
     )
