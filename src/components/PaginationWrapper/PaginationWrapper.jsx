b5cd1fefd78bfaf12baadfeea8e2930b152a6ada
Danyil <danilagarma1999@mail.ru>
2026-01-13 15:33:41 +0200

    refactor

--- src/components/PaginationWrapper/PaginationWrapper.jsx
+++ src/components/PaginationWrapper/PaginationWrapper.jsx
@@ -1,6 +1,4 @@
-import { formatDate } from "../../helpers/formatDate"
 import Pagination from "../Pagination/Pagination"
-import classes from "./style.module.css"
 
 const PaginationWrapper = ({ top, bottom, children, ...paginationProps }) => {
     return (
