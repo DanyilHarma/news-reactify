40595ce063a900e2b80b9c39fc9087a119030a3a
Danyil <danilagarma1999@mail.ru>
2026-01-14 18:52:28 +0200

    add rtk query and redux toolkit

--- src/components/LatestNews/LatestNews.tsx
+++ src/components/LatestNews/LatestNews.tsx
@@ -1,11 +1,9 @@
-import { getLatestNews } from "../../api/apiNews"
-import { useFetch } from "../../helpers/hooks/useFetch"
-import type { NewsApiResponse } from "../../interfaces"
+import { useGetLatestNewsQuery } from "../../store/services/newsApi"
 import BannersList from "../BannersList/BannersList"
 import classes from "./style.module.css"
 
 const LatestNews = () => {
-    const { data, isLoading } = useFetch<NewsApiResponse, void>(getLatestNews)
+    const { data, isLoading } = useGetLatestNewsQuery()
 
     return (
         <section className={classes.section}>
--- src/components/NewsByFilters/NewsByFilters.tsx
+++ src/components/NewsByFilters/NewsByFilters.tsx
@@ -1,42 +1,41 @@
-import { getNews } from "../../api/apiNews"
-import { PAGE_SIZE, TOTAL_PAGES } from "../../constants/constants"
+import { TOTAL_PAGES } from "../../constants/constants"
 import { useDebounce } from "../../helpers/hooks/useDebounce"
-import { useFetch } from "../../helpers/hooks/useFetch"
-import { useFilters } from "../../helpers/hooks/useFilters"
-import type { NewsApiResponse, ParamsType } from "../../interfaces"
+import { useAppDispatch, useAppSelector } from "../../store"
+import { useGetNewsQuery } from "../../store/services/newsApi"
+import { setFilters } from "../../store/slices/newsSlice"
 import NewsFilters from "../NewsFilters/NewsFilters"
 import NewsList from "../NewsList/NewsList"
 import PaginationWrapper from "../PaginationWrapper/PaginationWrapper"
 import classes from "./style.module.css"
 
 const NewsByFilters = () => {
-    const { filters, changeFilter } = useFilters({
-        page_number: 1,
-        page_size: PAGE_SIZE,
-        category: null,
-        keywords: ""
-    })
+    const dispatch = useAppDispatch();
+    const filters = useAppSelector(state => state.news.filters);
 
-    const deboundedKeywords = useDebounce(filters.keywords, 1500)
+    const deboundedKeywords = useDebounce(filters.keywords, 1500);
 
-    const { data, isLoading } = useFetch<NewsApiResponse, ParamsType>(getNews, {
+    const { data, isLoading } = useGetNewsQuery({
         ...filters,
         keywords: deboundedKeywords
-    })
+    });
 
     const handleNextPage = () => {
-        if (filters.page_number < TOTAL_PAGES) changeFilter("page_number", filters.page_number + 1)
-    }
+        if (filters.page_number < TOTAL_PAGES) {
+            dispatch(setFilters({ key: "page_number", value: filters.page_number + 1 }))
+        }
+    };
     const handlePrevPage = () => {
-        if (filters.page_number > 1) changeFilter("page_number", filters.page_number - 1)
-    }
+        if (filters.page_number > 1) {
+            dispatch(setFilters({ key: "page_number", value: filters.page_number - 1 }))
+        }
+    };
     const handlePageClick = (pageNumber: number) => {
-        changeFilter("page_number", pageNumber)
-    }
+        dispatch(setFilters({ key: "page_number", value: pageNumber }))
+    };
 
     return (
         <section className={classes.section}>
-            <NewsFilters filters={filters} changeFilter={changeFilter} />
+            <NewsFilters filters={filters} />
 
             <PaginationWrapper top bottom
                 totalPages={TOTAL_PAGES}
@@ -49,6 +48,6 @@ const NewsByFilters = () => {
             </PaginationWrapper>
         </section>
     )
-}
+};
 
-export default NewsByFilters
+export default NewsByFilters;
