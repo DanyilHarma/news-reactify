a86bfe38b2a5761b711277b5e8a93475bcf4cc84
Danyil <danilagarma1999@mail.ru>
2026-01-14 15:20:48 +0200

    migrate from js to ts

--- src/components/NewsFilters/NewsFilters.tsx
+++ src/components/NewsFilters/NewsFilters.tsx
@@ -1,12 +1,18 @@
 import { getCategories } from "../../api/apiNews"
 import { useFetch } from "../../helpers/hooks/useFetch"
+import type { CategoriesApiResponse, IFilters } from "../../interfaces"
 import Categories from "../Categories/Categories"
 import Search from "../Search/Search"
 import Slider from "../Slider/Slider"
 import classes from "./style.module.css"
 
-const NewsFilters = ({ filters, changeFilter }) => {
-    const { data: dataCetegories } = useFetch(getCategories)
+interface Props {
+    filters: IFilters;
+    changeFilter: (key: string, value: string | number | null) => void;
+}
+
+const NewsFilters = ({ filters, changeFilter }: Props) => {
+    const { data: dataCetegories } = useFetch<CategoriesApiResponse, void>(getCategories)
 
     return (
         <div className={classes.filters}>
